0ad811afb93ae496f14be561ee212e9d05beaf3d
Ivan Malison <IvanMalison@gmail.com>
2024-12-31 03:35:41 -0700

    Kill me

--- nixos/modules/services/hardware/nvidia-container-toolkit/cdi-generate.nix
+++ nixos/modules/services/hardware/nvidia-container-toolkit/cdi-generate.nix
@@ -8,6 +8,7 @@
   nvidia-driver,
   runtimeShell,
   writeScriptBin,
+  additionalEdit,
 }:
 let
   mkMount =
@@ -55,5 +56,5 @@ writeScriptBin "nvidia-cdi-generator" ''
   }
 
   cdiGenerate |
-    ${mountsToCommands mounts} > $RUNTIME_DIRECTORY/nvidia-container-toolkit.json
+  ${mountsToCommands mounts} | ${additionalEdit} > $RUNTIME_DIRECTORY/nvidia-container-toolkit.json
 ''
